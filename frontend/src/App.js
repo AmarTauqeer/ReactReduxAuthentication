ff0f8796ec74104acfae7f52035b80ce523e04ea
unknown <amar.tauqeer@gmail.com>
2020-05-03 19:40:04 +0200

    Navigation completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
 // App.js
 
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
-    return <div>React Redux Auth App</div>;
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </div>
+      </Router>
+    );
   }
 }
 
